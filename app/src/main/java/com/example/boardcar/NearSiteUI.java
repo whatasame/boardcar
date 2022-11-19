03d35c09a88a6f299b81c77214e8bead2d6fc53d
Narachan97 <qkrtkf3112@naver.com>
2022-11-19 22:57:18 +0900

    UI 메인(90%완성 세션되야 100%), 소모품 추적 완성

---
+++ app/src/main/java/com/example/boardcar/NearSiteUI.java
@@ -0,0 +1,14 @@
+package com.example.boardcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NearSiteUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_near_site_ui);
+    }
+}
--- app/src/main/java/com/example/boardcar/Utill.java
+++ app/src/main/java/com/example/boardcar/Utill.java
@@ -22,4 +22,5 @@ public class Utill  extends AppCompatActivity {
         Alert.show();
 
     }
+
 }
